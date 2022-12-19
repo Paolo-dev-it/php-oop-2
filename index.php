b0364b5a6debb8554b1137b37efbc780bf55ef19
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-12-19 19:03:32 +0100

    Modified array, changed mode of application. Project completed!

--- index.php
+++ index.php
@@ -14,19 +14,14 @@
 
     //var_dump( $category );
 
-    $prodottiCani = [
-        new Cibo( 'https://cdn.royalcanin-weshare-online.io/v2k6a2QBG95Xk-RBN9oV/v196/packshot-mini-ad-shn17', 'Royal Canin', 43.99, $category['cane'], 545, 'prosciutto, riso' ),
+    $prodotti = [
+        new Cibo('https://cdn.royalcanin-weshare-online.io/v2k6a2QBG95Xk-RBN9oV/v196/packshot-mini-ad-shn17', 'Royal Canin', 43.99, $category['cane'], 545, 'prosciutto, riso'),
 
-        new Cibo( 'https://shop-cdn-m.mediazs.com/bilder/almo/nature/holistic/medium/adult/con/manzo/fresco/5/800/68015_pla_almo_nature_holistic_adult_rind_reis_medium_746_12kg_dog_5.jpg', 'Almo Nature', 44.99, $category['cane'], 600, 'manzo, cereali' ),
+        new Cibo('https://shop-cdn-m.mediazs.com/bilder/almo/nature/holistic/medium/adult/con/manzo/fresco/5/800/68015_pla_almo_nature_holistic_adult_rind_reis_medium_746_12kg_dog_5.jpg', 'Almo Nature', 44.99, $category['cane'], 600, 'manzo, cereali'),
 
-    ];
+        new Accessori('https://static.bighunter.net/foto/1_Def_per_web_zoom/14089/14089.jpg', 'Voliera Ferplast Bella Casa', 184.99, $category['uccello'], 'legno', 'M: L83 x P67 x H153 cm '),
 
-    $prodottiUccelli = [ 
-        new Accessori('https://static.bighunter.net/foto/1_Def_per_web_zoom/14089/14089.jpg', 'Voliera Ferplast Bella Casa', 184.99, $category['uccello'], 'legno', 'M: L83 x P67 x H153 cm ')
-    ];
-
-    $prodottiGatti = [ 
-        new Giocattoli( 'https://i.ebayimg.com/images/g/03kAAOSwA4BbK4Gm/s-l500.jpg', 'Topini di Peluche Trixie', 4.99, $category['gatto'], 'Morbido con codina in corda', '8.5 cm x 10 cm' )
+        new Giocattoli('https://i.ebayimg.com/images/g/03kAAOSwA4BbK4Gm/s-l500.jpg', 'Topini di Peluche Trixie', 4.99, $category['gatto'], 'Morbido e salutare', '8.5 cm x 10 cm')
     ];
 
     // var_dump( $prodotti );
@@ -65,85 +60,115 @@
         <h1>Boolshop</h1>
         <h2>I nostri prodotti</h2>
         <div class="row">
-            <div class="col-4 ">
-               <?php foreach ($prodottiCani as $element) { ?>
-                   <div class="card">
-                    <img src= "<?php echo $element -> immagine ?>" class="card-img-top w-100" alt="...">
-                    <div class="card-body ">
-                        <h4 class="card-title text-dark text-uppercase"><?php echo $element -> nome ?></h4>
-                        <?php foreach($category as $elemento) ?>
-                            <i class="<?php echo $elemento -> icona ?>"></i>
-                            <p>
-                                <?php echo $elemento -> nome ?>
-                            </p> 
-                        <h5 class="card-title text-dark opacity-50">
-                        <?php echo $element -> prezzo  ?>
+            <?php
+            // Ciclo per scorrere l'array dei prodotti
+            foreach ($prodotti as $data) {
+                // Prodotto che appartiene alla categoria 'cane'
+                if ($data->category->nome == 'cane') {
+            ?>
+            <div class="col-3 mt-3">
+                <div class="card">
+                    <img class="progress" style="height: 304px;" src="<?php echo $data->immagine ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title text-uppercase">
+                            <?php echo $data->nome ?>
+                        </h4>
+                        <h6 class="card-title text-uppercase">
+                            <?php echo $data->category->nome ?>
+                            <i class="<?php echo $data->category->icona ?>"></i>
+                        </h6>
+                        <h5 class="card-title opacity-75">
+                            <?php echo $data->prezzo ?> $
                         </h5>
-                        <p class="card-text text-dark opacity-50">
-                        <?php echo $element -> pesonetto  ?>
-                        <p class="card-text text-dark opacity-50">
-                        <?php echo $element -> ingredienti  ?>
+                        <p class="card-text opacity-75">
+                            peso netto:
+                            <?php echo $data->pesonetto ?> g
                         </p>
+                        <p class="card-text opacity-75">
+                            ingredienti:
+                            <?php echo $data->ingredienti ?>
                         </p>
-                        
-                    </div>
-                </div> 
-        <?php } ?> 
-            </div>
-
-                    <div class="col-4">
-                        <?php foreach ($prodottiUccelli as $el) { ?>
-                        <div class="card">
-                            <img src= "<?php echo $el -> immagine ?>" class="card-img-top " alt="...">
-                            <div class="card-body ">
-                                <h4 class="card-title text-dark text-uppercase"><?php echo $el -> nome ?></h4>
-                                <?php foreach($category as $elemento) ?>
-                                    <i class="<?php echo $elemento -> icona ?>"></i>
-                                    <p>
-                                        <?php echo $elemento -> nome ?>
-                                    </p> 
-                                <h5 class="card-title text-dark opacity-50">
-                                    <?php echo $el -> prezzo  ?>
-                                </h5>
-                                <p class="card-text text-dark opacity-50">
-                                    <?php echo $el -> materiale  ?>
-                                </p>
-                                <p class="card-text text-dark opacity-50">
-                                    <?php echo $el -> dimensioni  ?>
-                                </p>
-                                
-                            </div>
-                        </div> 
-                        <?php } ?> 
-                    </div>
-                
-                    <div class="col-4">
-                           <?php foreach ($prodottiGatti as $data) { ?>
-                                <div class="card">
-                                    <img src= "<?php echo $data -> immagine ?>" class="card-img-top " alt="...">
-                                    <div class="card-body ">
-                                        <h4 class="card-title text-dark text-uppercase"><?php echo $data -> nome ?></h4>
-                                        <?php foreach($category as $elemento) ?>
-                                            <i class="<?php echo $elemento -> icona ?>"></i>
-                                            <p>
-                                                <?php echo $elemento -> nome ?>
-                                            </p> 
-                                        <h5 class="card-title text-dark opacity-50">
-                                            <?php echo $data -> prezzo  ?>
-                                        </h5>
-                                        <p class="card-text text-dark opacity-50">
-                                            <?php echo $data -> caratteristiche  ?>
-                                        </p>
-                                        <p class="card-text text-dark opacity-50">
-                                            <?php echo $data -> dimensioni  ?>
-                                        </p>
-                                        
-                                    </div>
-                                </div> 
-                                <?php } ?>
-                        </div> 
                     </div>
                 </div>
+            </div>
+            <?php
+                }
+            }
+            ?>
+
+            <?php
+
+            foreach ($prodotti as $data) {
+                // Prodotto che appartiene alla categoria 'uccello'
+                if ($data->category->nome == 'uccello') {
+            ?>
+            <div class="col-3 mt-3">
+                <div class="card">
+                    <img class="progress" style="height: 304px;" src="<?php echo $data->immagine ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title text-uppercase">
+                            <?php echo $data->nome ?>
+                        </h4>
+                        <h6 class="card-title text-uppercase">
+                            <?php echo $data->category->nome ?>
+                            <i class="<?php echo $data->category->icona ?>"></i>
+                        </h6>
+                        <h5 class="card-title opacity-75">
+                            <?php echo $data->prezzo ?> $
+                        </h5>
+                        <p class="card-text opacity-75">
+                            Materiale:
+                            <?php echo $data->materiale ?>
+                        </p>
+                        <p class="card-text opacity-75">
+                            Dimensioni:
+                            <?php echo $data->dimensioni ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <?php
+                }
+            }
+            ?>
+
+            <?php
+
+            foreach ($prodotti as $data) {
+                // Prodotto che appartiene alla categoria 'gatto'
+                if ($data->category->nome == 'gatto') {
+            ?>
+            <div class="col-3 mt-3">
+                <div class="card">
+                    <img class="progress" style="height: 304px;" src="<?php echo $data->immagine ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title text-uppercase">
+                            <?php echo $data->nome ?>
+                        </h4>
+                        <h6 class="card-title text-uppercase">
+                            <?php echo $data->category->nome ?> 
+                            <i class="<?php echo $data->category->icona ?>"></i>
+                        </h6>
+                        <h5 class="card-title opacity-75">
+                            <?php echo $data->prezzo ?> $
+                        </h5>
+                        <p class="card-text opacity-75">
+                            caratteristiche:
+                            <?php echo $data->caratteristiche ?>
+                        </p>
+                        <p class="card-text opacity-75">
+                            Dimensioni:
+                            <?php echo $data->dimensioni ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
 
     <!-- AXIOS -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
